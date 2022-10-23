078eee3acfb1aac99cd23746e27ac0b93c5d124e
Benton Diebold <89040723+CEOFYEAST@users.noreply.github.com>
2022-10-23 02:10:19 +0000

    initial branch setup

--- Hotel.java
+++
@@ -1,5 +0,0 @@
-public class Hotel extends Property{
-  public Hotel(Tile tile, Player owner){
-    super(tile, owner);
-  }
-}
--- House.java
+++
@@ -1,5 +0,0 @@
-public class House extends Property{
-  public House(Tile tile, Player owner){
-    super(tile, owner);
-  }
-}
