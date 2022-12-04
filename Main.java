107b3d825d994135311edf918900b7a4d02ccd42
Benton Diebold <89040723+CEOFYEAST@users.noreply.github.com>
2022-12-04 22:59:50 +0000

    further testing

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ class Main {
   public static void main(String[] args) {
     long initTime = System.currentTimeMillis();
     Misc.printBoard();
-    System.out.println("Diff: " + (currentTimeMillis() - initTime));
+    System.out.println("Diff: " + (System.currentTimeMillis() - initTime));
   }
 }
